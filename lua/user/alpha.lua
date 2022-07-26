f1d223d53206e01c74f2ea05d8c9f5c71d3fe37a
Axel García <axel.jacobo519@gmail.com>
2022-07-26 11:25:36 -0500

    thanks Chris ¯\_(ツ)_/¯

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -28,7 +28,7 @@ local function footer()
 	-- local fortune = handle:read("*a")
 	-- handle:close()
 	-- return fortune
-	return "chrisatmachine.com"
+	return "¯\\_(ツ)_/¯"
 end
 
 dashboard.section.footer.val = footer()
